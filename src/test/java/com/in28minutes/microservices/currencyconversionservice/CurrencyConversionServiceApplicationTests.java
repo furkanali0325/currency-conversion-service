deac8320ad8bf815b27286b639f5313571c1e222
furkan0325 <furkanali0325@gmail.com>
2023-03-28 09:47:59 +0530

    Adding test case

--- src/test/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
+++ src/test/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -1,13 +1,18 @@
 package com.in28minutes.microservices.currencyconversionservice;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
+@Slf4j
 class CurrencyConversionServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log.info("First test is under execution");
+		Assertions.assertEquals(true,true);
+
 	}
 
 }
